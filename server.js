c65beda81f93584c521fa30731dbeddf99706e95
Adham Ahmed <Tanticion@Adhams-MacBook-Pro.local>
2025-05-09 11:35:20 +0300

    Use dynamic port for hosting platform

--- server.js
+++ server.js
@@ -130,6 +130,8 @@ app.post("/api/submit", upload.single("image"), async (req, res) => {
 	}
 });
 
-app.listen(3000, () => {
-	console.log("Server is listening on port 3000");
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+	console.log(`Server is listening on port ${PORT}`);
 });
+
